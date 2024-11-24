2eb48d7031df07cc939c38dbb1e467ad52219c1b
Avi Rana <itzavi1718@gmail.com>
2024-11-24 13:35:09 +0530

    Working code

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,17 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 
+    public CharacterController controller;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
+    public float speed=6f;
+    public float turnSmoothTime =0.1f;
+    float turnSmoothVelocity;
 
-    // Update is called once per frame
     void Update()
     {
-=
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        if(direction.magnitude >= 0.1f) {
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothVelocity);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction*speed*Time.deltaTime);
+        }
     }
 }
