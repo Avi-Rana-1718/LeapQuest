cb6f06cac8f6ee5f953711ce5de8ad99168d3323
Avi Rana <itzavi1718@gmail.com>
2024-11-26 21:41:31 +0530

    Animation fixed

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,10 +6,17 @@ public class Player : MonoBehaviour
 {
 
     public CharacterController controller;
+    public Animator animator;
 
     public float speed=6f;
+    public float jumpSpeed = 9f;
+    public float ySpeed;
     public float turnSmoothTime =0.1f;
     float turnSmoothVelocity;
+    
+    void Start() {
+        animator= GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -23,8 +30,20 @@ public class Player : MonoBehaviour
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothVelocity);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
+            animator.SetBool("isMoving", true);
             controller.Move(direction*speed*Time.deltaTime);
         }
+        animator.SetBool("isMoving", false);
+
+    ySpeed+=Physics.gravity.y * Time.deltaTime;
+        if(Input.GetButtonDown("Jump")) {
+            Debug.Log("Jump");
+            ySpeed=-0.5f; 
+        }
+
+        Vector3 vel =direction * speed;
+        vel.y=ySpeed;
+        controller.Move(direction*Time.deltaTime);
+
     }
 }
